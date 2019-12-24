1f7abed0c226a1634742db006e4177f2ba950f83
AlexHHC <HHC@AlexHHCs-MacBook-Pro.local>
2019-12-24 11:08:11 +0800

    publish

---
+++ precache-manifest.12776b259c8289684d3f4f4581a034e8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8711889d188d34c02d351cbf8f99dd78",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b027ace122256451ce54",
+    "url": "/static/css/2.2ee1cb70.chunk.css"
+  },
+  {
+    "revision": "c2d1c1eb5f39372e2ab2",
+    "url": "/static/css/main.d4d2c0e2.chunk.css"
+  },
+  {
+    "revision": "b027ace122256451ce54",
+    "url": "/static/js/2.9154f1bc.chunk.js"
+  },
+  {
+    "revision": "87984cad984dae3222b2a1d0861003a4",
+    "url": "/static/js/2.9154f1bc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c2d1c1eb5f39372e2ab2",
+    "url": "/static/js/main.7e2a50dd.chunk.js"
+  },
+  {
+    "revision": "716870da583956ea451c",
+    "url": "/static/js/runtime-main.7ea44dfd.js"
+  }
+]);
